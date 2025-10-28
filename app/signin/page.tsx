1406cc746fe0a4aa8c1b8929c4094d26b8cbae81
Dami Kang <dami@SJHs-MacBook.local>
2025-10-28 18:54:23 +0900

    added w-full on signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@ import SignInCard from "@/components/signin-card";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black dark:bg-black">
+    <div className="flex w-full min-h-screen items-center justify-center bg-black dark:bg-black">
       <main>
           <SignInCard />
       </main>
